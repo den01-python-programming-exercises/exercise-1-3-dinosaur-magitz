a3ebca93cc17f86a5e2542f4c128448875795054
Matt Gitzendanner <magitz@ufl.edu>
2021-05-28 18:23:24 +0000

    fix output

--- src/exercise.py
+++ src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #write your code below this line
   print('Once upon a time')
   print('there was')
-  print('a dinosaur!')
+  print('a dinosaur')
   
 if __name__ == '__main__':
     main()
